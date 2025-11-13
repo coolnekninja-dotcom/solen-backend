ce64409dc37e835f0fd3b1622d45982d01717443
coolnekninja-dotcom <coolnekninja@gmail.com>
2025-11-12 23:39:26 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const PRICE_MAP = {
+  "white-sweatpants": {
+    S: "price_1SSTdD6VqDaMUiptLNJ61G1p",
+    M: "price_1SSTfr6VqDaMUiptfHrrKGM9",
+    L: "price_1SSTiC6VqDaMUiptdlnIXEnm"
+  },
+  "black-sweatpants": {
+    S: "price_1SSRfT6VqDaMUiptecql1O1w",
+    M: "price_1SSTRj6VqDaMUiptfVyDtfZD",
+    L: "price_1SSTVy6VqDaMUipt5Me5cnpB"
+  }
+};
